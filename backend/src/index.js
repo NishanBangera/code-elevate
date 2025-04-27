de2be660656da8e82e479cb2fcb7cf375a66d632
Nishan Bangera <nishanbangera4321@gmail.com>
2025-04-27 20:51:29 +0530

    Add Prisma setup with user model and database migration; update dependencies in package.json and package-lock.json; configure .gitignore for generated files; enhance server response and add database connection utility.

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,12 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the CodeElevate");
+});
+
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on port 5000");
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
