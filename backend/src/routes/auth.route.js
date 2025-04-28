22b3b281dc0e58c009e24dc9b05418abf65bb9f3
Nishan Bangera <nishanbangera4321@gmail.com>
2025-04-28 11:50:43 +0530

    Add authentication features: implement user registration, login, and logout; integrate JWT for token management; add cookie handling with cookie-parser

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { registerUser, loginUser, logoutUser, checkAuth } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+const authRoutes = express.Router();
+
+authRoutes.post("/register", registerUser);
+authRoutes.post("/login", loginUser);
+authRoutes.post("/logout", authMiddleware, logoutUser);
+authRoutes.get("/check", authMiddleware, checkAuth);
+
+export default authRoutes;
